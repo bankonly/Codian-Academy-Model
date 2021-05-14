5ea4e2386a614efbc5d87770068afba245c2b262
bankonly <for21bn01@gmail.com>
2021-05-15 00:14:59 +0700

    addLikeGallery

--- models/parent_model.js
+++ models/parent_model.js
@@ -1,89 +1,101 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { BRANCH_MODEL_NAME } = require("./branch_model");
+const { TEACHER_MODEL_NAME } = require("./teacher_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-    },
-    first_name: {
-      type: String,
-      required: true,
-    },
-    last_name: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    img: {
-      type: String,
-      required: true,
-    },
-    nationality: {
-      type: String,
-      required: true,
-    },
-    tribe: {
-      // Zon phao
-      type: String,
-      default: null,
-    },
-    working_place: {
-      type: String,
-      default: null,
-    },
-    job_title: {
-      type: String,
-      default: null,
-    },
-    education_id: {
-      type: Schema.Types.ObjectId,
-      ref: 'parent_education',
-      default: null,
-    },
-    graduated_year: {
-      type: Number,
-      default: null,
-    },
-    phone_number: [
-      {
-        type: Number,
-        required: true,
-      },
-    ],
-    gender: {
-      type: String,
-      enum: ['male', 'female'],
-      default: null,
-    },
-    village: { type: String, default: null },
-    district: { type: String, default: null },
-    province: { type: String, default: null },
-    parent_status: {
-      type: String,
-      default: null,
-    },
-    student_id: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'student',
-      },
-    ],
-    refresh_token: {
-      type: String,
-      default: null,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		first_name: {
+			type: String,
+			required: true,
+		},
+		last_name: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		img: {
+			type: String,
+			required: true,
+		},
+		nationality: {
+			type: String,
+			required: true,
+		},
+		tribe: {
+			// Zon phao
+			type: String,
+			default: null,
+		},
+		working_place: {
+			type: String,
+			default: null,
+		},
+		job_title: {
+			type: String,
+			default: null,
+		},
+		education_id: {
+			type: Schema.Types.ObjectId,
+			ref: "parent_education",
+			default: null,
+		},
+		created_by: {
+			type: Schema.Types.ObjectId,
+			ref: TEACHER_MODEL_NAME,
+			default: null,
+		},
+		branch_id: {
+			type: Schema.Types.ObjectId,
+			ref: BRANCH_MODEL_NAME,
+			default: null,
+		},
+		graduated_year: {
+			type: Number,
+			default: null,
+		},
+		phone_number: [
+			{
+				type: Number,
+				required: true,
+			},
+		],
+		gender: {
+			type: String,
+			enum: ["male", "female"],
+			default: null,
+		},
+		village: { type: String, default: null },
+		district: { type: String, default: null },
+		province: { type: String, default: null },
+		parent_status: {
+			type: String,
+			default: null,
+		},
+		student_id: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "student",
+			},
+		],
+		refresh_token: {
+			type: String,
+			default: null,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
-const model_name = 'parent';
+const model_name = "parent";
 module.exports = mongoose.model(model_name, schema, model_name);
 module.exports.PARENT_MODEL_NAME = model_name;
