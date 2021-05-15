a62c56304c9efe0c8c34c7c9edcfcbb63eb614bd
bankonly <for21bn01@gmail.com>
2021-05-15 22:05:10 +0700

    modifyNoti

--- models/notification_model.js
+++ models/notification_model.js
@@ -1,52 +1,58 @@
 const mongoose = require("mongoose");
+const { BRANCH_MODEL_NAME } = require("./branch_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    teacher_id: {
-      type: Schema.Types.ObjectId,
-      ref: "teacher",
-    },
-    parent_id: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "parent",
-      },
-    ],
-    title: {
-      type: String,
-      required: true,
-    },
-    desc: {
-      type: String,
-      required: true,
-    },
-    img: [
-      {
-        type: String,
-        required: true,
-      },
-    ],
-    readed: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "parent",
-        default: [],
-      },
-    ],
-    deleted: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "parent",
-        default: [],
-      },
-    ],
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+	{
+		teacher_id: {
+			type: Schema.Types.ObjectId,
+			ref: "teacher",
+		},
+		parent_id: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "parent",
+			},
+		],
+		branch_id: {
+			type: Schema.Types.ObjectId,
+			ref: BRANCH_MODEL_NAME,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		desc: {
+			type: String,
+			required: true,
+		},
+		img: [
+			{
+				type: String,
+				required: true,
+			},
+		],
+		readed: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "parent",
+				default: [],
+			},
+		],
+		deleted: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "parent",
+				default: [],
+			},
+		],
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
 const model_name = "notification";
