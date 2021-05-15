185ddeff99a8ae06bf98c1dbf91b984f46003409
bankonly <for21bn01@gmail.com>
2021-05-15 21:13:00 +0700

    modifyParentAddress

--- models/class_model.js
+++ models/class_model.js
@@ -1,24 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { BRANCH_MODEL_NAME } = require("./branch_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    year_id: {
-      type: Schema.Types.ObjectId,
-      ref: 'year',
-      require: true,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		year_id: {
+			type: Schema.Types.ObjectId,
+			ref: "year",
+			require: true,
+		},
+		branch_id: {
+			ref: BRANCH_MODEL_NAME,
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
-const model_name = 'class';
+const model_name = "class";
 module.exports = mongoose.model(model_name, schema, model_name);
 module.exports.CLASSES_MODEL_NAME = model_name;
--- models/student_model.js
+++ models/student_model.js
@@ -1,51 +1,57 @@
-const mongoose = require('mongoose');
-const { CLASSES_MODEL_NAME } = require('./class_model');
+const mongoose = require("mongoose");
+const { BRANCH_MODEL_NAME } = require("./branch_model");
+const { CLASSES_MODEL_NAME } = require("./class_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    first_name: {
-      type: String,
-      required: true,
-    },
-    last_name: {
-      type: String,
-      required: true,
-    },
-    nick_name: {
-      type: String,
-      required: true,
-    },
-    img: {
-      type: String,
-      default: null,
-    },
-    gender: {
-      type: String,
-      enum: ['male', 'female'],
-      required: true,
-    },
-    date_of_birth: {
-      type: Date,
-      required: true,
-    },
-    identify_id: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    class_id: {
-      type: Schema.Types.ObjectId,
-      ref: CLASSES_MODEL_NAME,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+	{
+		first_name: {
+			type: String,
+			required: true,
+		},
+		last_name: {
+			type: String,
+			required: true,
+		},
+		nick_name: {
+			type: String,
+			required: true,
+		},
+		img: {
+			type: String,
+			default: null,
+		},
+		gender: {
+			type: String,
+			enum: ["male", "female"],
+			required: true,
+		},
+		date_of_birth: {
+			type: Date,
+			required: true,
+		},
+		identify_id: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		branch_id: {
+			ref: BRANCH_MODEL_NAME,
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		class_id: {
+			type: Schema.Types.ObjectId,
+			ref: CLASSES_MODEL_NAME,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
-const model_name = 'student';
+const model_name = "student";
 module.exports = mongoose.model(model_name, schema, model_name);
 module.exports.STUDENT_MODEL_NAME = model_name;
