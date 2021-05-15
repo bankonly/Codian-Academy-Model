1ceff61f548fcbebb8958de80568f7fdcbf98972
bankonly <for21bn01@gmail.com>
2021-05-15 21:42:09 +0700

    modifySubjectModel

--- models/subject_model.js
+++ models/subject_model.js
@@ -4,29 +4,27 @@ const { BRANCH_MODEL_NAME } = require("./branch_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    branch_id: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: BRANCH_MODEL_NAME,
-        required: true,
-      },
-    ],
-    year_id: {
-      type: Schema.Types.ObjectId,
-      ref: YEAR_MODEL_NAME,
-      required: true,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		branch_id: {
+			type: Schema.Types.ObjectId,
+			ref: BRANCH_MODEL_NAME,
+			required: true,
+		},
+		year_id: {
+			type: Schema.Types.ObjectId,
+			ref: YEAR_MODEL_NAME,
+			required: true,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
 const model_name = "subject";
