f399cec38da35af28b1b3890299a220fcf191dfb
bankonly <for21bn01@gmail.com>
2021-05-15 21:49:02 +0700

    modifySubjectModel

--- models/year_model.js
+++ models/year_model.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { BRANCH_MODEL_NAME } = require("./branch_model");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		branch_id: {
+			type: Schema.Types.ObjectId,
+			ref: BRANCH_MODEL_NAME,
+			required: true,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
-const model_name = 'year';
+const model_name = "year";
 module.exports = mongoose.model(model_name, schema, model_name);
 module.exports.YEAR_MODEL_NAME = model_name;
