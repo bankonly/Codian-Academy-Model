844861ba3b7f0b61887909fd2f5e37e2691e3bb6
bankonly <for21bn01@gmail.com>
2021-05-25 23:57:03 +0700

    updateAddtuitionfee

--- models/fee_model.js
+++ models/fee_model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const { BRANCH_MODEL_NAME } = require("./branch_model");
-const { PARENT_MODEL_NAME } = require("./parent_model");
 const { STUDENT_MODEL_NAME } = require("./student_model");
 const { TEACHER_MODEL_NAME } = require("./teacher_model");
 
@@ -38,11 +37,6 @@ const schema = new Schema(
         teacher_id: {
 			type: Schema.Types.ObjectId,
 			ref: TEACHER_MODEL_NAME,
-			required: true,
-		},
-        parent_id: {
-			type: Schema.Types.ObjectId,
-			ref: PARENT_MODEL_NAME,
 			required: true,
 		},
 		deleted_at: {
