87456fc6021bfa41fb380a114382529f92f1b780
mekky <mackychinma@gmail.com>
2021-06-26 20:21:43 +0700

    score_type

---
+++ models/score_type.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schema = new Schema(
+  {
+    la_name: {
+      type: String,
+      required: true,
+    },
+    en_name: {
+      type: String,
+      required: true,
+    },
+    deleted_at: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+
+const model_name = "score_type";
+module.exports = mongoose.model(model_name, schema, model_name);
+module.exports.SCORE_TYPE_MODEL_NAME = model_name;
